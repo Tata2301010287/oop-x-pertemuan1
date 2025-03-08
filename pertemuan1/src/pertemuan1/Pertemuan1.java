c04c1cc7af45742025edfa1056a986985e1794be
Tata2301010287 <tataabur@gmail.com>
2025-03-08 17:08:45 +0800

    Project 1

---
+++ pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,21 @@
+
+package pertemuan1;
+
+/**
+ *
+ * @author Antonia Ttarianingsih Abur
+ * TGL: 8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Object Oriented Programming");
+       System.out.println("Aku Cinta Instiki");
+       System.out.print("Bhineka Tunggal IKa ");
+       System.out.println("Berbeda-beda Tapi Tetap Satu");
+    }
+    
+}
